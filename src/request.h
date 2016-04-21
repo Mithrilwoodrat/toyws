c56d934975dca235c5f4b15e7ea037f5667a8efa
mithrilwoodrat <mithrilwoodrat@gmail.com>
2016-04-21 22:07:41 +0800

    add request struct

--- src/request.h
+++ src/request.h
@@ -3,14 +3,10 @@
 
 #include "toyws.h"
 
-typedef struct request_msg{
-    request_line start_line;
-    request_header header;
-    request_body body;
-}requst_msg;
 
 #define MAXLEN 100
 #define URLLEN 1000
+
 typedef struct request_line {
     char method[MAXLEN];
     char request_uri[URLLEN];
@@ -23,9 +19,42 @@ typedef struct header_field {
     struct header_field *next;
 }header_field;
 
-#define MAXSIZE 100
 typedef struct requst_header {
-    header_field header_fields[MAXSIZE];
-}
+    ssize_t linenums;
+    header_field head;
+}request_header;
+
+#define MAXBODYLEN 1000
+typedef struct request_body {
+    ssize_t content_length;
+    char _body[MAXBODYLEN];
+}request_body;
+
+
+
+typedef struct request_msg{
+    request_line start_line;
+    request_header header;
+    request_body body;
+}requst_msg;
+
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
